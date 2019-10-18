cfea83f7ef864a147ead88cad844baa82be441df
Pat P <sell445@gmail.com>
2019-10-18 13:32:49 -0400

    set up fetch and postform

--- reduxcrashcourse/src/App.js
+++ reduxcrashcourse/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PostForm from './components/Postform';
 
 import Posts from './components/Posts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <Posts />
-      </header>
+      <main>
+        <PostForm />
+        <hr></hr>
+        <Posts />
+      </main>
     </div>
   );
 }
