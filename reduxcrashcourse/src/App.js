df6060f999d763e26c2ca55eb126cf4721d4169b
Pat P <sell445@gmail.com>
2019-10-18 12:49:31 -0400

    Initial commit.  Set up React app & components folder

---
+++ reduxcrashcourse/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Posts from './components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <Posts />
+      </header>
+    </div>
+  );
+}
+
+export default App;
